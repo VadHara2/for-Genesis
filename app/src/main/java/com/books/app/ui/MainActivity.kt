ec3431dec1680e17c47be65997bc09cefe250596
Vadym <VadHara2@gmail.com>
2021-08-14 11:16:26 +0300

    add splash screen

---
+++ app/src/main/java/com/books/app/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.books.app.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.books.app.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/books/app/ui/splash/SplashActivity.kt
@@ -0,0 +1,24 @@
+package com.books.app.ui.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.books.app.databinding.ActivitySplashBinding
+
+import com.books.app.ui.MainActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
+    }
+}
